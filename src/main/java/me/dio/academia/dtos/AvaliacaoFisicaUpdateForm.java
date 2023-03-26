c36fc3e66cbf16200597131b57614e1e4bc5a4ca
Fabiano Batista <fabianobatista@outlook.com>
2023-03-26 12:21:26 -0300

    add validations for AvaliacaoFisica

--- src/main/java/me/dio/academia/dtos/AvaliacaoFisicaUpdateForm.java
+++ src/main/java/me/dio/academia/dtos/AvaliacaoFisicaUpdateForm.java
@@ -1,6 +1,15 @@
 package me.dio.academia.dtos;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Positive;
+
 public record AvaliacaoFisicaUpdateForm(
+    @Positive(message = "${validatedValue}' precisa ser positivo.")    
     double pesoAtual,
+
+    @Positive(message = "${validatedValue}' precisa ser positivo.")
+    @DecimalMin(value = "2.30", message = "'${validatedValue}' precisa ser no mínimo {value}.")
+    @DecimalMax(value = "1.20", message = "'${validatedValue}' precisa ser no máximo {value}.")
     double alturaAtual) {
 }
